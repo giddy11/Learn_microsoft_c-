6c5d54fb0c80aa548195da68eb9580d20a10ed3d
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-12-07 11:00:26 +0100

    gr

--- Microsoft_C#/StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
+++ Microsoft_C#/StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d8b25365b7e7b22055e3b3cfcf49f44e7e2e38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
