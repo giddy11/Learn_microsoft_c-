d905e7ddd6327d327da0beb65e7df2c80b67a70a
Gideon Edoghotu <edoghotugiddy@gmail.com>
2024-01-23 17:34:05 +0100

    done

---
+++ Microsoft_C#/ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using TypeConversion.Study;
+using me =  TypeConversion.Study.TypeConversion;
+
+Console.WriteLine("Hello, World!");
+
+me.MainMethod();
---
+++ Microsoft_C#/TypeConversion.Study/TypeConversion.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace TypeConversion.Study
+{
+    public class TypeConversion
+    {
+
+        public static void MainMethod()
+        {
+            int a = 90;
+
+            byte b;
+
+            b = (byte)a;
+
+            System.Convert.ToByte(b);
+            Convert.ToDouble(b);
+            Console.WriteLine(b);
+        }
+    }
+}
