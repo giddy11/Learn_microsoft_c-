c70f41d27fcd06cc73c4f27a52a9e79260ce2385
Gideon Edoghotu <edoghotugiddy@gmail.com>
2024-02-01 16:26:47 +0100

    updated

---
+++ Microsoft_C#/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Library.Structures;
+
+Console.WriteLine("Hello, World!");
+
+Category category = new Category() { CategoryID = 20, CategoryName = "General"};
+int len = category.GetCategoryNameLength();
+
+Console.WriteLine(len);
---
+++ Microsoft_C#/Library.Structures/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Structures
+{
+    public struct Category
+    {
+        public int GetCategoryNameLength()
+        {
+            return this.CategoryName.Length;
+        }
+
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
