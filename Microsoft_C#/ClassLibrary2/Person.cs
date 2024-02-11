d96331de5a1b90582c255b210974fe71b0b0f3bb
giddy11 <edoghotugiddy@gmail.com>
2024-02-11 23:39:07 +0100

    done

---
+++ Microsoft_C#/ClassLibrary2/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary2
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            var per = (Person)obj;
+            if(this.Name == per.Name)
+                return true;
+
+            return false;
+        }
+    }
+}
